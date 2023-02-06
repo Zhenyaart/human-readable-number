a1c4dc7552e14e58fc91ac74be4302e88aad00df
Zhenyaart <logitech22139@mail.ru>
2023-02-06 20:14:10 +0300

    human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+const a = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+const b = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+
+
 module.exports = function toReadable (number) {
-  
+    
+    if (number < 20) {
+        return a[number];
+    }
+    const c = number%10;
+    if (number < 100) {
+        return b[~~(number/10)-2] + (c? " " + a[c]: "");
+    }
+    if (number < 1000) {
+        return a[~~(number/100)] +" hundred" + (number%100 == 0? "": " " + toReadable(number%100));
+    }
+
+    return toReadable(~~(number/1000)) + " thousand" + (number%1000 != 0? " " + toReadable(number%1000): "");
 }
+
+
